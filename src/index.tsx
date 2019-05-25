a78e5a037a8056c1f7fb7d92417c334571bb8b2b
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-05-24 22:17:26 -0400

    Split icon CSS into their own files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import "md-icons.css";
+import "mdi-icons.css";
+import "index.css";
+import App from "App";
+import * as serviceWorker from "serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
