6082765c51764ddd9a926f7516f095696e2809ec
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-05-25 00:16:40 -0400

    Add hot-reloading

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,20 @@ import "index.css";
 import App from "App";
 import * as serviceWorker from "serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const MOUNT_NODE = document.getElementById("root");
+
+const render = () => {
+  ReactDOM.render(<App />, MOUNT_NODE);
+};
+
+if (module.hot) {
+  module.hot.accept("App", () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE!);
+    render();
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
