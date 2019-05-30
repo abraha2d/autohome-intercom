274a9c26e3a6e77acfdd52f94041f1f791e92f91
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-05-29 22:54:31 -0400

    Functional global back button on all pages except home

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,49 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, match, withRouter } from "react-router";
+import { History, Location } from "history";
 import "./App.css";
 
 import apps from "apps";
 import Home from "apps/Home";
 
-function App() {
-  return (
-    <div className="main-container">
-      <div className="status-bar">
-        <div className="col left">May 22</div>
-        <div className="col center">12:34 am</div>
-        <div className="col right">
-          75º <div className="mdi mdi-weather-sunny md-18" aria-hidden="true" />
-        </div>
-      </div>
-      <div className="content-container">
-        <Switch>
-          <Route exact path={Home.path} component={Home.app} />
-          {apps.map(app => (
-            <Route key={app.name} path={app.path} component={app.app} />
-          ))}
-        </Switch>
+type Props = {
+  history: History;
+  location: Location;
+  match: match;
+};
+
+const App: React.FC<Props> = ({ history }) => (
+  <div className="main-container">
+    <div className="status-bar">
+      <Switch>
+        <Route
+          exact
+          path={Home.path}
+          component={() => <div className="col left">May 22</div>}
+        />
+        <Route
+          component={() => (
+            <div
+              className="col left icon-button mdi md-18 mdi-keyboard-backspace"
+              onClick={() => history.go(-1)}
+            />
+          )}
+        />
+      </Switch>
+      <div className="col center">12:34 am</div>
+      <div className="col right">
+        75º <div className="mdi md-18 mdi-weather-sunny" />
       </div>
     </div>
-  );
-}
+    <div className="content-container">
+      <Switch>
+        <Route exact path={Home.path} component={Home.app} />
+        {apps.map(app => (
+          <Route key={app.name} path={app.path} component={app.app} />
+        ))}
+      </Switch>
+    </div>
+  </div>
+);
 
-export default App;
+export default withRouter(App);
