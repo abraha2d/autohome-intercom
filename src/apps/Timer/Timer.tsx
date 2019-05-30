60a51220a49ed8b3ad631f2bc2f6770398d398c2
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-05-29 22:46:17 -0400

    Make stub apps use scrollbar

--- src/apps/Timer/Timer.tsx
+++ src/apps/Timer/Timer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { withRouter } from "react-router-dom";
 import { History } from "history";
+import ScrollBar from "components/ScrollBar";
 
 type Props = {
   history: History;
@@ -9,15 +10,7 @@ type Props = {
 const Timer: React.FC<Props> = ({ history }) => (
   <div className="content-container">
     <div className="content">Hi, this is Timer!</div>
-    <div className="scroll-container">
-      <div className="scroll-button icon-button material-icons md-48">
-        keyboard_arrow_up
-      </div>
-      <div className="scroll-indicator" />
-      <div className="scroll-button icon-button material-icons md-48">
-        keyboard_arrow_down
-      </div>
-    </div>
+    <ScrollBar onUpClick={() => {}} onDownClick={() => {}} barParams={false} />
   </div>
 );
 
