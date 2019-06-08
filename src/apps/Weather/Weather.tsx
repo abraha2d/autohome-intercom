abf725a6d84c254a87cba04d9e7b39577f587660
Kevin Abraham <abraha2d@users.noreply.github.com>
2019-06-08 14:11:37 -0400

    [weather] Add loading icon, make widget load weather as well

--- src/apps/Weather/Weather.tsx
+++ src/apps/Weather/Weather.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { geolocated, GeolocatedProps } from "react-geolocated";
 import { match, withRouter } from "react-router-dom";
 import { History, Location } from "history";
+
+import { Icon } from "@mdi/react";
 import { mdiCancel, mdiWeatherPartlycloudy } from "@mdi/js";
+import ReactLoading from "react-loading";
 import "weathericons/css/weather-icons.min.css";
 
 import { getIcon, getShortDay } from "./utils";
 import "./Weather.css";
-import Icon from "@mdi/react";
 
 type Props = {
   history: History;
@@ -20,21 +22,17 @@ type State = {
   gotForecast: boolean;
   gotStations: boolean;
   gotObservations: boolean;
-  forecast: any;
-  stations: any;
-  observations: any;
 };
 
 class Weather extends React.Component<Props & GeolocatedProps> {
   readonly state: State = {
     gotForecast: false,
     gotStations: false,
-    gotObservations: false,
-    forecast: {},
-    stations: {},
-    observations: {}
+    gotObservations: false
   };
 
+  static forecast: any = false;
+  static stations: any = false;
   static observations: any = false;
 
   render() {
@@ -44,13 +42,80 @@ class Weather extends React.Component<Props & GeolocatedProps> {
       coords,
       isWidget
     } = this.props;
-    const {
-      gotForecast,
-      gotStations,
-      gotObservations,
-      forecast,
-      stations
-    } = this.state;
+    const { gotForecast, gotStations, gotObservations } = this.state;
+
+    let message;
+    if (!isGeolocationAvailable) message = "Geolocation not supported!";
+    else if (!isGeolocationEnabled) message = "Geolocation not enabled!";
+    else if (!coords) {
+      message = "Finding location...";
+      gotForecast && this.setState({ gotForecast: false });
+      gotStations && this.setState({ gotStations: false });
+      gotObservations && this.setState({ gotObservations: false });
+    } else if (!gotForecast) {
+      message = "Getting forecast...";
+      fetch(
+        `https://api.weather.gov/points/${coords.latitude},${
+          coords.longitude
+        }/forecast`
+      )
+        .then(response => response.json())
+        .then(response => {
+          Weather.forecast = response;
+          this.setState({ gotForecast: true, forecast: response });
+        });
+    } else if (!gotStations) {
+      message = "Finding stations...";
+      fetch(
+        `https://api.weather.gov/points/${coords.latitude},${
+          coords.longitude
+        }/stations`
+      )
+        .then(response => response.json())
+        .then(response => {
+          Weather.stations = response;
+          this.setState({ gotStations: true });
+        });
+    } else if (!gotObservations) {
+      const station = Weather.stations.features[0].properties.stationIdentifier;
+      message = `Found station ${station}...`;
+      fetch(`https://api.weather.gov/stations/${station}/observations`)
+        .then(response => response.json())
+        .then(response => {
+          for (let i = 0; i < response.features.length; i++) {
+            if (response.features[i].properties.temperature.value) {
+              Weather.observations = response.features[i].properties;
+            }
+          }
+          this.setState({ gotObservations: true });
+        });
+    }
+
+    const todayFC = Weather.observations;
+    console.debug("[weather] Observation:", todayFC);
+
+    const nextFCs: any[] = [];
+    if (Weather.forecast.properties) {
+      if (!Weather.forecast.properties.periods[0].isDaytime) {
+        nextFCs.push({ day: {} });
+      }
+
+      Weather.forecast.properties.periods.forEach((period: any) => {
+        if (period.isDaytime) {
+          nextFCs.push({ day: period });
+        } else {
+          nextFCs[nextFCs.length - 1].night = period;
+        }
+      });
+      nextFCs.splice(6);
+      nextFCs[0].day.name = "Today";
+
+      if (!nextFCs[0].day.icon && todayFC.icon) {
+        todayFC.icon = todayFC.icon.replace("day", "night");
+      }
+
+      console.debug("[weather] Forecast:", nextFCs);
+    }
 
     if (isWidget) {
       if (Weather.observations) {
@@ -66,83 +131,14 @@ class Weather extends React.Component<Props & GeolocatedProps> {
         );
       } else {
         return (
-          <span>
-            --º&nbsp;&nbsp;
-            <Icon path={mdiCancel} size={"24px"} />
-          </span>
+          <ReactLoading
+            type="spin"
+            height={"24px"}
+            width={"24px"}
+            color="#fff"
+          />
         );
       }
-    }
-
-    let message;
-    if (!isGeolocationAvailable) message = "Geolocation not supported!";
-    else if (!isGeolocationEnabled) message = "Geolocation not enabled!";
-    else if (!coords) {
-      message = "Finding location...";
-      gotForecast && this.setState({ gotForecast: false });
-      gotStations && this.setState({ gotStations: false });
-      gotObservations && this.setState({ gotObservations: false });
-    } else if (!gotForecast) {
-      message = "Getting weather forecast...";
-      fetch(
-        `https://api.weather.gov/points/${coords.latitude},${
-          coords.longitude
-        }/forecast`
-      )
-        .then(response => response.json())
-        .then(response =>
-          this.setState({ gotForecast: true, forecast: response })
-        );
-    } else if (!gotStations) {
-      message = "Finding nearby weather stations...";
-      fetch(
-        `https://api.weather.gov/points/${coords.latitude},${
-          coords.longitude
-        }/stations`
-      )
-        .then(response => response.json())
-        .then(response =>
-          this.setState({ gotStations: true, stations: response })
-        );
-    } else if (!gotObservations) {
-      const station = stations.features[0].properties.stationIdentifier;
-      message = `Getting current weather from station ${station}...`;
-      fetch(`https://api.weather.gov/stations/${station}/observations`)
-        .then(response => response.json())
-        .then(response => {
-          for (let i = 0; i < response.features.length; i++) {
-            if (response.features[i].properties.temperature.value) {
-              Weather.observations = response.features[i].properties;
-            }
-          }
-          this.setState({ gotObservations: true, observations: response });
-        });
-    }
-
-    const todayFC = Weather.observations;
-    console.debug("[weather] Observation:", todayFC);
-
-    const nextFCs: any[] = [];
-    if (forecast.properties) {
-      if (!forecast.properties.periods[0].isDaytime) {
-        nextFCs.push({ day: {} });
-      }
-
-      forecast.properties.periods.forEach((period: any) => {
-        if (period.isDaytime) {
-          nextFCs.push({ day: period });
-        } else {
-          nextFCs[nextFCs.length - 1].night = period;
-        }
-      });
-      nextFCs.splice(6);
-      nextFCs[0].day.name = "Today";
-
-      if (!nextFCs[0].day.icon && todayFC.icon) {
-        todayFC.icon = todayFC.icon.replace("day", "night");
-      }
-
-      console.debug("[weather] Forecast:", nextFCs);
     }
 
     return (
@@ -163,7 +159,12 @@ class Weather extends React.Component<Props & GeolocatedProps> {
               </div>
             ) : (
               <div className="weather-main">
-                <Icon path={mdiCancel} size={"48px"} color={"white"} />
+                <ReactLoading
+                  type="spin"
+                  height={"48px"}
+                  width={"48px"}
+                  color="#fff"
+                />
                 <div>-- ºF</div>
                 <div className="weather-message">
                   <span>{message}</span>
